e899e5a844c7df60570cc87f217358c16ff1e1d1
jasmeendesai <130737953+jasmeendesai@users.noreply.github.com>
2024-05-10 15:17:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next)=>{
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cors({
-    origin : "http://localhost:3000",
+    origin : "https://amazing-eclair-c3891d.netlify.app",
 }))
 app.use(cookieParser())
 
